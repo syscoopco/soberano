b91f6bada9274d2a5d627a4fd422ce2bcd2b67a9
P F Josue <josue.portal@manicato.net>
2024-08-25 23:24:42 -0400

    name changed to tcptool.
    SPI now depends on StockChange instead of StockRecord (snapshot of
    current stock taken on closure). The latter is problematic with closure
    cancellations and inventory recalculations. The former takes into
    account the time inventory operations are carried out.

--- soberano/src/main/java/co/syscoop/soberano/composers/BillButtonComposer.java
+++ soberano/src/main/java/co/syscoop/soberano/composers/BillButtonComposer.java
@@ -1,11 +1,13 @@
 package co.syscoop.soberano.composers;
 
+import java.math.BigDecimal;
+
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zul.Box;
 import org.zkoss.zul.Intbox;
-
+import co.syscoop.soberano.domain.tracked.Order;
 import co.syscoop.soberano.ui.helper.BusinessActivityTrackedObjectFormHelper;
 import co.syscoop.soberano.ui.helper.OrderFormHelper;
 
@@ -26,6 +28,18 @@ public class BillButtonComposer extends BusinessActivityTrackedObjectButtonCompo
 	@Listen("onClick = button#btnBill")
     public void btnBill_onClick() {
 	
-		Executions.sendRedirect("/bill.zul?id=" + ((Intbox) boxDetails.query("#intObjectId")).getValue().toString());
+		Integer orderId = ((Intbox) boxDetails.query("#intObjectId")).getValue();		
+		Order order = new Order(orderId);
+		try {
+			if (order.getCanceledRunsCount().compareTo(new BigDecimal(0)) > 0) {
+				Executions.sendRedirect("/bill.zul?id=" + orderId.toString());
+			}
+			else {
+				Executions.sendRedirect("/cash_register.zul?oid=" + orderId);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.fillInStackTrace();
+		}
 	}
 }
