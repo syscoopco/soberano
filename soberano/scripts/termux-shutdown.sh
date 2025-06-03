cd4d36f71f93d1a6ca5aa9b8680db89439e15dfb
P F Josue <josue.portal@manicato.net>
2025-06-03 11:45:44 -0400

    It was changed the termux deploying scripts and instructions.

--- soberano/scripts/termux-shutdown.sh
+++ soberano/scripts/termux-shutdown.sh
@@ -1,6 +1,6 @@
 /data/data/com.termux/files/usr/opt/apacheds/bin/apacheds.sh default stop
 
-/data/data/com.termux/files/usr/opt/tomcat9/bin/shutdown.sh
+/data/data/com.termux/files/usr/opt/tomcat11/bin/shutdown.sh
 
 pg_ctl -D /data/data/com.termux/files/home/postgresql_data stop
 
--- soberano/scripts/termux-startup.sh
+++ soberano/scripts/termux-startup.sh
@@ -1,6 +1,6 @@
 /data/data/com.termux/files/usr/opt/apacheds/bin/apacheds.sh default start
 
-/data/data/com.termux/files/usr/opt/tomcat9/bin/startup.sh
+/data/data/com.termux/files/usr/opt/tomcat11/bin/startup.sh
 
 pg_ctl -D /data/data/com.termux/files/home/postgresql_data start
 
